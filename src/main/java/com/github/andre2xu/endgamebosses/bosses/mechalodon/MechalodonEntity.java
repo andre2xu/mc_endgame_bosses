7e4d2787e7d77fbe8933da833a94c7f7033bc932
andre2xu <>
2025-04-08 13:35:20 +0100

    Created the entity class for Mechalodon.

---
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/MechalodonEntity.java
@@ -0,0 +1,35 @@
+package com.github.andre2xu.endgamebosses.bosses.mechalodon;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.FlyingMob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.util.GeckoLibUtil;
+
+public class MechalodonEntity extends FlyingMob implements GeoEntity {
+    private final AnimatableInstanceCache geo_cache = GeckoLibUtil.createInstanceCache(this);
+
+
+
+    public MechalodonEntity(EntityType<? extends FlyingMob> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    public static AttributeSupplier createAttributes() {
+        return FlyingMob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 10) // CHANGE LATER
+                .build();
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {}
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return this.geo_cache;
+    }
+}
