dc0fbeebfc68990d9125f18fbf39aaf815fa46a7
andre2xu <>
2025-04-08 13:37:20 +0100

    Registered the Mechalodon as an entity type.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/BossRegistry.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/BossRegistry.java
@@ -1,13 +1,19 @@
 package com.github.andre2xu.endgamebosses.bosses;
 
 import com.github.andre2xu.endgamebosses.EndgameBosses;
+import com.github.andre2xu.endgamebosses.bosses.mechalodon.MechalodonEntity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class BossRegistry {
     public static DeferredRegister<EntityType<?>> BOSS_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, EndgameBosses.MODID);
+
+    // TYPES
+    public static RegistryObject<EntityType<MechalodonEntity>> MECHALODON = BOSS_TYPES.register("mechalodon", () -> EntityType.Builder.of(MechalodonEntity::new, MobCategory.MONSTER).build("mechalodon"));
 
 
 
