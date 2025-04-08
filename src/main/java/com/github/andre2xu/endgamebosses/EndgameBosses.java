8085f8eb5c1a2738ea177bca541d202f61a93f01
andre2xu <>
2025-04-08 16:04:28 +0100

    Added events to detect if the Ender Dragon is dead.

--- src/main/java/com/github/andre2xu/endgamebosses/EndgameBosses.java
+++ src/main/java/com/github/andre2xu/endgamebosses/EndgameBosses.java
@@ -4,10 +4,19 @@ import com.github.andre2xu.endgamebosses.bosses.BossRegistry;
 import com.github.andre2xu.endgamebosses.bosses.mechalodon.MechalodonEntity;
 import com.github.andre2xu.endgamebosses.bosses.mechalodon.MechalodonRenderer;
 import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.end.EndDragonFight;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -43,6 +52,33 @@ public class EndgameBosses {
 
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {}
+
+    @SubscribeEvent
+    public void onPlayerChangedDimension(final PlayerEvent.PlayerChangedDimensionEvent event) {
+        ResourceKey<Level> level = event.getTo();
+
+        // if a player goes to the End, check if the Ender Dragon has already been killed before
+        if (level.equals(Level.END)) {
+            Player player = event.getEntity();
+
+            if (player.level() instanceof ServerLevel serverLevel) {
+                EndDragonFight dragon_fight = serverLevel.getDragonFight();
+
+                if (dragon_fight != null && dragon_fight.hasPreviouslyKilledDragon()) {
+                    System.out.println("Endgame bosses can now be fought");
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onEntityDeath(final LivingDeathEvent event) {
+        LivingEntity entity = event.getEntity();
+
+        if (entity.getType() == EntityType.ENDER_DRAGON) {
+            System.out.println("Endgame bosses can now be fought");
+        }
+    }
 
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
