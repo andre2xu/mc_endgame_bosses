cd03ce33b6b4b59ea16151cb1da402ad2e203a81
andre2xu <>
2025-04-09 13:07:39 +0100

    Created a custom target goal for the Mechalodon and registered it.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/MechalodonEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/MechalodonEntity.java
@@ -1,14 +1,23 @@
 package com.github.andre2xu.endgamebosses.bosses.mechalodon;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.FlyingMob;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.util.GeckoLibUtil;
+
+import java.util.function.Predicate;
 
 public class MechalodonEntity extends FlyingMob implements GeoEntity {
     private final AnimatableInstanceCache geo_cache = GeckoLibUtil.createInstanceCache(this);
@@ -40,5 +49,39 @@ public class MechalodonEntity extends FlyingMob implements GeoEntity {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.geo_cache;
+    }
+
+
+
+    // AI
+    @Override
+    protected void registerGoals() {
+        // find and select a target
+        this.targetSelector.addGoal(1, new SelectTargetGoal(this));
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+
+        if (this.level() instanceof ServerLevel) {
+            System.out.println(this.getTarget());
+        }
+    }
+
+
+
+    // CUSTOM GOALS
+    private static class SelectTargetGoal extends NearestAttackableTargetGoal<Player> {
+        public SelectTargetGoal(Mob pMob) {
+            this(pMob, Player.class, 10, true, false, null);
+        }
+
+        public SelectTargetGoal(Mob pMob, Class<Player> pTargetType, int pRandomInterval, boolean pMustSee, boolean pMustReach, @Nullable Predicate<LivingEntity> pTargetPredicate) {
+            super(pMob, pTargetType, pRandomInterval, pMustSee, pMustReach, pTargetPredicate);
+
+            final double MAX_TARGET_DISTANCE = 50d; // blocks
+            this.targetConditions = TargetingConditions.forCombat().range(MAX_TARGET_DISTANCE).selector(pTargetPredicate);
+        }
     }
 }
