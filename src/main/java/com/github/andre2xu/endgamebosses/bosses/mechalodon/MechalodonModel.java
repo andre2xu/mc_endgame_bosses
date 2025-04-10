a8d0216b42253d7ea7158b0b66418edb05873fe3
andre2xu <>
2025-04-10 12:33:30 +0100

    Overrode the Mechalodon model's 'setCustomAnimations' method and added code to rotate the body bone using the synched body pitch entity data.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/MechalodonModel.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/MechalodonModel.java
@@ -2,7 +2,11 @@ package com.github.andre2xu.endgamebosses.bosses.mechalodon;
 
 import com.github.andre2xu.endgamebosses.EndgameBosses;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.model.GeoModel;
+
+import java.util.Optional;
 
 public class MechalodonModel extends GeoModel<MechalodonEntity> {
     @Override
@@ -18,5 +22,13 @@ public class MechalodonModel extends GeoModel<MechalodonEntity> {
     @Override
     public ResourceLocation getAnimationResource(MechalodonEntity animatable) {
         return ResourceLocation.fromNamespaceAndPath(EndgameBosses.MODID, "animations/entity/mechalodon/mechalodon.animation.json");
+    }
+
+    @Override
+    public void setCustomAnimations(MechalodonEntity animatable, long instanceId, AnimationState<MechalodonEntity> animationState) {
+        super.setCustomAnimations(animatable, instanceId, animationState);
+
+        Optional<GeoBone> body_bone = getBone("body");
+        body_bone.ifPresent(body -> body.setRotX(animatable.getBodyPitch()));
     }
 }
