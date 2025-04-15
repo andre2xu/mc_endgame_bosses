27f41017f9990bf02e2f1c3009373b28b3993875
andre2xu <>
2025-04-15 21:40:18 +0100

    Added code to disable the Mechalodon's body pitch rotation when the 'face_up' and 'face_down' animations are playing.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/MechalodonModel.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/MechalodonModel.java
@@ -2,10 +2,12 @@ package com.github.andre2xu.endgamebosses.bosses.mechalodon;
 
 import com.github.andre2xu.endgamebosses.EndgameBosses;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.animation.AnimationProcessor;
 import software.bernie.geckolib.animation.AnimationState;
 import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.model.GeoModel;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public class MechalodonModel extends GeoModel<MechalodonEntity> {
@@ -28,7 +30,23 @@ public class MechalodonModel extends GeoModel<MechalodonEntity> {
     public void setCustomAnimations(MechalodonEntity animatable, long instanceId, AnimationState<MechalodonEntity> animationState) {
         super.setCustomAnimations(animatable, instanceId, animationState);
 
-        Optional<GeoBone> body_bone = getBone("body");
-        body_bone.ifPresent(body -> body.setRotX(animatable.getBodyPitch()));
+        boolean rotate_body = true;
+
+        // check what animation is running and decide whether to allow the rotation of the body or not
+        AnimationProcessor.QueuedAnimation current_animation = animationState.getController().getCurrentAnimation();
+
+        if (current_animation != null) {
+            String running_animation_name = current_animation.animation().name();
+
+            if (Objects.equals(running_animation_name, "animation.mechalodon.face_up") || Objects.equals(running_animation_name, "animation.mechalodon.face_down")) {
+                rotate_body = false;
+            }
+        }
+
+        // this is for 'LookControl.setLookAt' to work
+        if (rotate_body) {
+            Optional<GeoBone> body_bone = getBone("body");
+            body_bone.ifPresent(body -> body.setRotX(animatable.getBodyPitch()));
+        }
     }
 }
