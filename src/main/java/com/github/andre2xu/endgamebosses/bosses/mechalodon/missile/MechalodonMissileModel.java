0b9aa9e494b1796d410ec526115e6bf1f5e67daf
andre2xu <>
2025-04-18 11:39:41 +0100

    The 'setCustomAnimations' method of the Mechalodon missile's model class can now rotate the body.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/missile/MechalodonMissileModel.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/missile/MechalodonMissileModel.java
@@ -2,7 +2,11 @@ package com.github.andre2xu.endgamebosses.bosses.mechalodon.missile;
 
 import com.github.andre2xu.endgamebosses.EndgameBosses;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.model.GeoModel;
+
+import java.util.Optional;
 
 public class MechalodonMissileModel extends GeoModel<MechalodonMissileEntity> {
     @Override
@@ -18,5 +22,13 @@ public class MechalodonMissileModel extends GeoModel<MechalodonMissileEntity> {
     @Override
     public ResourceLocation getAnimationResource(MechalodonMissileEntity animatable) {
         return ResourceLocation.fromNamespaceAndPath(EndgameBosses.MODID, "animations/entity/mechalodon/mechalodon_missile.animation.json");
+    }
+
+    @Override
+    public void setCustomAnimations(MechalodonMissileEntity animatable, long instanceId, AnimationState<MechalodonMissileEntity> animationState) {
+        super.setCustomAnimations(animatable, instanceId, animationState);
+
+        Optional<GeoBone> body_bone = getBone("body");
+        body_bone.ifPresent(body -> body.setRotX(animatable.getBodyPitch()));
     }
 }
