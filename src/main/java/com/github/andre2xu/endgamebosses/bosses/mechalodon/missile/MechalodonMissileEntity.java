0d5e0f6c22e73da160eea48f9a242555184c89ab
andre2xu <>
2025-04-18 10:55:59 +0100

    Deleted the overrides in the Mechalodon missile entity class that were no longer required and also the 'setTarget' method. Deleted unused imports too.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/missile/MechalodonMissileEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/missile/MechalodonMissileEntity.java
@@ -1,14 +1,10 @@
 package com.github.andre2xu.endgamebosses.bosses.mechalodon.missile;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
@@ -17,7 +13,6 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 public class MechalodonMissileEntity extends PathfinderMob implements GeoEntity {
     private final AnimatableInstanceCache geo_cache = GeckoLibUtil.createInstanceCache(this);
     private int auto_detonation_countdown = 20 * 5; // 5 seconds
-    private LivingEntity target = null;
 
 
 
@@ -46,24 +41,6 @@ public class MechalodonMissileEntity extends PathfinderMob implements GeoEntity 
 
 
 
-    // DATA
-    @Override
-    protected void defineSynchedData(SynchedEntityData.@NotNull Builder pBuilder) {
-
-    }
-
-    @Override
-    protected void readAdditionalSaveData(@NotNull CompoundTag pCompound) {
-
-    }
-
-    @Override
-    protected void addAdditionalSaveData(@NotNull CompoundTag pCompound) {
-
-    }
-
-
-
     // AI
     private void decrementAutoDetonationCountdown() {
         if (this.auto_detonation_countdown > 0) {
@@ -71,20 +48,16 @@ public class MechalodonMissileEntity extends PathfinderMob implements GeoEntity 
         }
     }
 
-    private void setTarget(LivingEntity target) {
-        // call this before spawning a missile
-
-        this.target = target;
-    }
-
     @Override
     public void tick() {
         super.tick();
 
-        if (this.auto_detonation_countdown > 0 && this.target != null) {
+        if (this.auto_detonation_countdown > 0) {
             System.out.println("Homing in on target");
         }
         else {
+            System.out.println("KABOOM");
+
             // delete missile from game
             this.discard();
             return;
