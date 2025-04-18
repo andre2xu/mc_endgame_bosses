aa6fe69c50ad2c91a81e63fbb4fbd23c88dda7f2
andre2xu <>
2025-04-18 10:40:35 +0100

    Added an animation file for the Mechalodon missile.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/missile/MechalodonMissileModel.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mechalodon/missile/MechalodonMissileModel.java
@@ -17,6 +17,6 @@ public class MechalodonMissileModel extends GeoModel<MechalodonMissileEntity> {
 
     @Override
     public ResourceLocation getAnimationResource(MechalodonMissileEntity animatable) {
-        return null;
+        return ResourceLocation.fromNamespaceAndPath(EndgameBosses.MODID, "animations/entity/mechalodon/mechalodon_missile.animation.json");
     }
 }
