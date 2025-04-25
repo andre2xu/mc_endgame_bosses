8c53cbe3bc7919bed9731670519df07a3b856e9e
andre2xu <>
2025-04-26 00:07:01 +0100

    Made the Tragon a multipart entity and used hitbox entities for its parts. Also, created a method for updating the positions of the hitboxes.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/TragonEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/TragonEntity.java
@@ -1,17 +1,28 @@
 package com.github.andre2xu.endgamebosses.bosses.tragon;
 
+import com.github.andre2xu.endgamebosses.bosses.misc.HitboxEntity;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.entity.PartEntity;
 import net.minecraftforge.fluids.FluidType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.Objects;
+
 public class TragonEntity extends PathfinderMob implements GeoEntity {
+    private final PartEntity<?>[] hitboxes;
+
     // ANIMATIONS
     private final AnimatableInstanceCache geo_cache = GeckoLibUtil.createInstanceCache(this);
 
@@ -19,6 +30,14 @@ public class TragonEntity extends PathfinderMob implements GeoEntity {
 
     public TragonEntity(EntityType<? extends PathfinderMob> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+
+        this.hitboxes = new PartEntity[] {
+                new HitboxEntity(this, "fire_head_neck", 2, 2),
+                new HitboxEntity(this, "lightning_head_neck", 2, 2),
+                new HitboxEntity(this, "ice_head_neck", 2, 2)
+        };
+
+        this.setId(ENTITY_COUNTER.getAndAdd(this.hitboxes.length + 1) + 1);
     }
 
     public static AttributeSupplier createAttributes() {
@@ -39,6 +58,46 @@ public class TragonEntity extends PathfinderMob implements GeoEntity {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.geo_cache;
+    }
+
+
+
+    // EXTRA HITBOXES
+    @Override
+    public void setId(int pId) {
+        super.setId(pId);
+
+        // FIX: giving the hitboxes their own ids is required for hurt detection to work properly (see EnderDragon class)
+        for (int i = 0; i < this.hitboxes.length; i++) {
+            this.hitboxes[i].setId(pId + i + 1);
+        }
+    }
+
+    @Override
+    public boolean isMultipartEntity() {
+        return true;
+    }
+
+    @Override
+    public @Nullable PartEntity<?>[] getParts() {
+        return this.hitboxes;
+    }
+
+    public void updateHitboxPosition(String boneName, Vec3 bonePos) {
+        // this is called in the 'setCustomAnimations' method of the model class because that's where the bone positions can be accessed
+
+        Level level = this.level();
+
+        for (PartEntity<?> hitbox : this.hitboxes) {
+            if (Objects.equals(((HitboxEntity) hitbox).getBoneName(), boneName)) {
+                if (!level.isClientSide) {
+                    hitbox.setPos(bonePos);
+                }
+                else {
+                    hitbox.moveTo(bonePos);
+                }
+            }
+        }
     }
 
 
