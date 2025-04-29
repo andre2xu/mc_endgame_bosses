e4d1183e70e5623bf07af5896e92f41e8c2dff0c
andre2xu <>
2025-04-29 17:14:37 +0100

    Created a class to represent a Tragon head.

---
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/TragonHead.java
@@ -0,0 +1,30 @@
+package com.github.andre2xu.endgamebosses.bosses.tragon.heads;
+
+public class TragonHead {
+    private float health;
+    private boolean is_alive = true;
+
+    public TragonHead(float maxHealth) {
+        this.health = maxHealth;
+    }
+
+    public float getHealth() {
+        return this.health;
+    }
+
+    public boolean isAlive() {
+        return this.is_alive;
+    }
+
+    public void hurt(float damageAmount) {
+        if (this.health > 0) {
+            this.health = this.health - damageAmount;
+
+            if (this.health <= 0) {
+                this.is_alive = false;
+
+                this.health = 0; // prevent health from having a negative value
+            }
+        }
+    }
+}
