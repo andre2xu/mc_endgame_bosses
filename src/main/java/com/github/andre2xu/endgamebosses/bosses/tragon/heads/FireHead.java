b032a3acec4292639c1a4009734a8cdd2eb04dcf
andre2xu <>
2025-05-01 17:17:16 +0100

    Implemented the shooting logic in the Tragon's fireballs attack.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/FireHead.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/FireHead.java
@@ -4,6 +4,8 @@ import com.github.andre2xu.endgamebosses.bosses.tragon.TragonEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.Random;
+
 public class FireHead extends TragonHead {
     public FireHead(TragonEntity parent, float maxHealth) {
         super(parent, maxHealth);
@@ -17,10 +19,22 @@ public class FireHead extends TragonHead {
     private static class Fireballs implements TragonHeadAttack {
         private final TragonEntity tragon;
         private LivingEntity target = null;
+        private int shoot_cooldown = 0; // no cooldown for first fireball
+        private int num_of_fireballs_to_shoot = 0;
         private boolean attack_is_finished = false;
 
         public Fireballs(TragonEntity tragon) {
             this.tragon = tragon;
+        }
+
+        private void decrementShootCooldown() {
+            if (this.shoot_cooldown > 0) {
+                this.shoot_cooldown--;
+            }
+        }
+
+        private void resetShootCooldown() {
+            this.shoot_cooldown = 20; // 1 second
         }
 
         private boolean canAttack() {
@@ -30,6 +44,8 @@ public class FireHead extends TragonHead {
         @Override
         public void resetAttack() {
             this.target = null;
+            this.shoot_cooldown = 0;
+            this.num_of_fireballs_to_shoot = 0;
             this.attack_is_finished = false;
         }
 
@@ -37,6 +53,9 @@ public class FireHead extends TragonHead {
         public void start() {
             // save a reference of the target to avoid having to call 'this.tragon.getTarget' which can sometimes return null
             this.target = this.tragon.getTarget();
+
+            // decide how many fireballs will be shot
+            this.num_of_fireballs_to_shoot = new Random().nextInt(3, 6); // 3 to 5 fireballs will be shot
         }
 
         @Override
@@ -47,7 +66,20 @@ public class FireHead extends TragonHead {
         @Override
         public void tick() {
             if (this.canAttack()) {
-                System.out.println("Shooting fireballs");
+                if (this.num_of_fireballs_to_shoot > 0) {
+                    if (this.shoot_cooldown > 0) {
+                        this.decrementShootCooldown();
+                    }
+                    else {
+                        this.num_of_fireballs_to_shoot--;
+
+                        this.resetShootCooldown();
+                    }
+                }
+                else {
+                    // stop attack
+                    this.attack_is_finished = true;
+                }
             }
             else {
                 // cancel attack if target doesn't exist, is dead, or is in creative/spectator mode
