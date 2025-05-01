5cd8751465f8fc2e22f40ca4aa3412c4f77b4dc1
andre2xu <>
2025-05-01 11:54:33 +0100

    Created an attack for the Tragon's fire head and added it to its attacks list.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/FireHead.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/FireHead.java
@@ -5,5 +5,31 @@ import com.github.andre2xu.endgamebosses.bosses.tragon.TragonEntity;
 public class FireHead extends TragonHead {
     public FireHead(TragonEntity parent, float maxHealth) {
         super(parent, maxHealth);
+
+        this.addAttack(new Fireballs(parent));
+    }
+
+
+
+    // ATTACKING
+    private static class Fireballs implements TragonHeadAttack {
+        private final TragonEntity tragon;
+        private boolean attack_is_finished = false;
+
+        public Fireballs(TragonEntity tragon) {
+            this.tragon = tragon;
+        }
+
+        @Override
+        public void tick() {
+            System.out.println("Shooting fireballs");
+
+            this.attack_is_finished = true;
+        }
+
+        @Override
+        public boolean canUse() {
+            return !this.attack_is_finished;
+        }
     }
 }
