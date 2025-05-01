dfffcb6fffc23a239e8c7437e0305f29a31189e0
andre2xu <>
2025-05-01 11:39:02 +0100

    Created an attack system in the TragonHead class.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/TragonHead.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/TragonHead.java
@@ -2,10 +2,16 @@ package com.github.andre2xu.endgamebosses.bosses.tragon.heads;
 
 import com.github.andre2xu.endgamebosses.bosses.tragon.TragonEntity;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TragonHead {
     private final TragonEntity parent;
     private float health;
     private boolean has_taken_damage = false; // this flag ensures that the Tragon's 'readAdditionalSaveData' method only updates the health of a head if it has already taken damage
+    private final ArrayList<TragonHeadAttack> all_attacks = new ArrayList<>();
+    private boolean attack_is_finished = false;
+    private TragonHeadAttack attack = null;
 
     public TragonHead(TragonEntity parent, float maxHealth) {
         this.parent = parent;
@@ -40,5 +46,48 @@ public class TragonHead {
 
     public void setHasTakenDamage(boolean flag) {
         this.has_taken_damage = flag;
+    }
+
+
+
+    // ATTACKING
+    protected void addAttack(TragonHeadAttack attack) {
+        this.all_attacks.add(attack);
+    }
+
+    public void chooseAttack() {
+        // this method should be called in the 'start' method of a goal since it decides what attack the 'attackTick' method will run
+
+        if (!this.all_attacks.isEmpty()) {
+            int index = new Random().nextInt(0, this.all_attacks.size());
+
+            this.attack = this.all_attacks.get(index);
+        }
+    }
+
+    public boolean isFinishedAttacking() {
+        // this method should be used to check if 'attackTick' can still be called
+
+        return this.attack_is_finished;
+    }
+
+    public void attackTick() {
+        // this method should be called in the 'tick' method of a goal. It must be paired with 'isFinishedAttacking', otherwise it will never stop being called
+
+        if (this.attack != null && this.attack.canUse()) {
+            this.attack.tick();
+        }
+        else {
+            this.attack = null;
+
+            // mark attack as finished since there's either no attack at all or the attack chosen can't run
+            this.attack_is_finished = true;
+        }
+    }
+
+    protected interface TragonHeadAttack {
+        void tick();
+
+        boolean canUse();
     }
 }
