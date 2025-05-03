25d437b1f575c8fa69ee6191af9f382b50f5b22e
andre2xu <>
2025-05-03 10:52:32 +0100

    Implemented a delay in the Tragon's frost breath attack.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/IceHead.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/heads/IceHead.java
@@ -17,10 +17,21 @@ public class IceHead extends TragonHead {
     private static class FrostBreath implements TragonHeadAttack {
         private final TragonEntity tragon;
         private LivingEntity target = null;
+        private int attack_delay = 0;
         private boolean attack_is_finished = false;
 
         public FrostBreath(TragonEntity tragon) {
             this.tragon = tragon;
+        }
+
+        private void decrementAttackDelay() {
+            if (this.attack_delay > 0) {
+                this.attack_delay--;
+            }
+        }
+
+        private void resetAttackDelay() {
+            this.attack_delay = 20 * 2; // 2 seconds
         }
 
         @Override
@@ -38,6 +49,9 @@ public class IceHead extends TragonHead {
         public void start() {
             // save a reference of the target to avoid having to call 'this.tragon.getTarget' which can sometimes return null
             this.target = this.tragon.getTarget();
+
+            // add a delay before the attack
+            this.resetAttackDelay();
         }
 
         @Override
@@ -47,7 +61,20 @@ public class IceHead extends TragonHead {
 
         @Override
         public void tick() {
+            if (this.canAttack()) {
+                if (this.attack_delay > 0) {
+                    this.decrementAttackDelay();
+                }
+                else {
+                    System.out.println("USING FROST BREATH");
 
+                    this.attack_is_finished = true;
+                }
+            }
+            else {
+                // cancel attack if ice head is dead, target doesn't exist, target is dead, target is too close, or target is in creative/spectator mode
+                this.attack_is_finished = true;
+            }
         }
 
         @Override
