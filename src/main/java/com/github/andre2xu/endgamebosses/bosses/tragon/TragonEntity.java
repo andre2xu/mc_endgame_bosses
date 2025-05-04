cf79dfa356be71349aae4a3cea2a425972a39ba7
andre2xu <>
2025-05-04 12:44:17 +0100

    Made the Tragon's 1 head attack always select the fire head for development.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/TragonEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/TragonEntity.java
@@ -787,7 +787,15 @@ public class TragonEntity extends PathfinderMob implements GeoEntity {
                 ArrayList<TragonHead> alive_heads = this.tragon.getAliveHeads();
 
                 // choose the Tragon head that will attack
-                this.attacking_head = alive_heads.get(new Random().nextInt(0, alive_heads.size()));
+                // this.attacking_head = alive_heads.get(new Random().nextInt(0, alive_heads.size()));
+
+                // temp
+                for (TragonHead head : alive_heads) {
+                    if (head instanceof FireHead) {
+                        this.attacking_head = head;
+                        break;
+                    }
+                }
 
                 this.attacking_head.chooseAttack();
             }
