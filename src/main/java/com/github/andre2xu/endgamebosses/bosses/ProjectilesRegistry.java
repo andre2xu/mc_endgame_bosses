1da8325a5edc49de00256cecc86feb6fc841e6c2
andre2xu <>
2025-05-05 11:43:49 +0100

    Registered the Tragon's icicle as a projectile.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/ProjectilesRegistry.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/ProjectilesRegistry.java
@@ -2,6 +2,7 @@ package com.github.andre2xu.endgamebosses.bosses;
 
 import com.github.andre2xu.endgamebosses.EndgameBosses;
 import com.github.andre2xu.endgamebosses.bosses.mechalodon.missile.MechalodonMissileEntity;
+import com.github.andre2xu.endgamebosses.bosses.tragon.icicle.TragonIcicleEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,6 +15,8 @@ public class ProjectilesRegistry {
 
     // TYPES
     public static RegistryObject<EntityType<MechalodonMissileEntity>> MECHALODON_MISSILE = PROJECTILE_TYPES.register("mechalodon_missile", () -> EntityType.Builder.of(MechalodonMissileEntity::new, MobCategory.MISC).sized(1f, 0.5f).build("mechalodon_missile"));
+
+    public static RegistryObject<EntityType<TragonIcicleEntity>> TRAGON_ICICLE = PROJECTILE_TYPES.register("tragon_icicle", () -> EntityType.Builder.of(TragonIcicleEntity::new, MobCategory.MISC).sized(1.5f, 7f).build("tragon_icicle"));
 
 
 
