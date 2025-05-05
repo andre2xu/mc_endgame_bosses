b94d0a344072f80c1ab2e44bd15113640c71af81
andre2xu <>
2025-05-05 12:52:54 +0100

    The Tragon's icicle now plays sounds when hitting the ground and generates particles.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/icicle/TragonIcicleEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/tragon/icicle/TragonIcicleEntity.java
@@ -1,6 +1,10 @@
 package com.github.andre2xu.endgamebosses.bosses.tragon.icicle;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
@@ -8,6 +12,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -43,6 +48,35 @@ public class TragonIcicleEntity extends PathfinderMob implements GeoEntity {
 
 
     // AI
+    private void generateLandingParticles(Vec3 landingPos, int radius) {
+        if (this.level() instanceof ServerLevel server_level) {
+            BlockPos center_pos = BlockPos.containing(landingPos).below();
+
+            int depth = 3;
+
+            for (int x=-radius; x <= radius; x++) {
+                for (int y=-depth; y <= depth; y++) {
+                    for (int z=-radius; z <= radius; z++) {
+                        BlockPos block_pos = center_pos.offset(x, y, z);
+                        BlockState block_state = server_level.getBlockState(block_pos);
+
+                        boolean above_block_is_air = server_level.getBlockState(block_pos.above()).isAir();
+
+                        if (above_block_is_air) {
+                            server_level.sendParticles(
+                                    new BlockParticleOption(ParticleTypes.BLOCK, block_state),
+                                    block_pos.getX(), block_pos.getY(), block_pos.getZ(),
+                                    10, // particle count
+                                    0, 0.5, 0, // make particles move upwards
+                                    0.1 // speed
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     @Override
     protected void checkFallDamage(double pY, boolean pOnGround, @NotNull BlockState pState, @NotNull BlockPos pPos) {
         // immune to fall damage
@@ -57,5 +91,25 @@ public class TragonIcicleEntity extends PathfinderMob implements GeoEntity {
 
         // invincible against entities
         return false;
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+
+        if (this.verticalCollision) {
+            float noise_volume = 1.5f;
+            this.playSound(SoundEvents.GLASS_BREAK, noise_volume, 1f);
+            this.playSound(SoundEvents.GENERIC_EXPLODE.get(), noise_volume, 1f);
+
+            Vec3 landing_pos = this.position();
+
+            int radius = 5; // radius of blocks affected by land
+
+            this.generateLandingParticles(landing_pos, radius);
+
+            // disappear from game
+            this.discard();
+        }
     }
 }
