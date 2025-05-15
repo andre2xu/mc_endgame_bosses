90c252d42939bf561ce5546e23f4d7658f3a3853
andre2xu <>
2025-05-15 11:19:44 +0100

    Created a custom nearest attackable target goal for Mama and registered it.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/mama/MamaEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/mama/MamaEntity.java
@@ -1,17 +1,20 @@
 package com.github.andre2xu.endgamebosses.bosses.mama;
 
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.util.GeckoLibUtil;
+
+import java.util.function.Predicate;
 
 public class MamaEntity extends PathfinderMob implements GeoEntity {
     // ANIMATIONS
@@ -63,9 +66,36 @@ public class MamaEntity extends PathfinderMob implements GeoEntity {
     }
 
     @Override
+    protected void registerGoals() {
+        // find and select a target
+        this.targetSelector.addGoal(3, new SelectTargetGoal(this));
+    }
+
+    @Override
     public void aiStep() {
         super.aiStep();
+    }
 
-        this.triggerAnim("movement_trigger_anim_controller", "walk");
+
+
+    // CUSTOM GOALS
+    private static class SelectTargetGoal extends NearestAttackableTargetGoal<Player> {
+        public SelectTargetGoal(Mob pMob) {
+            // this is a custom constructor made to reduce the amount of parameters. It doesn't override any constructor from the parent
+
+            this(pMob, Player.class, 10, true, false, null);
+        }
+
+        public SelectTargetGoal(Mob pMob, Class<Player> pTargetType, int pRandomInterval, boolean pMustSee, boolean pMustReach, @Nullable Predicate<LivingEntity> pTargetPredicate) {
+            // this is the main constructor where the target conditions are set (see NearestAttackableTargetGoal). It was overridden to increase how far Mama can spot targets
+
+            super(pMob, pTargetType, pRandomInterval, pMustSee, pMustReach, pTargetPredicate);
+
+            final double MAX_TARGET_DISTANCE = 40d; // blocks
+            this.targetConditions = TargetingConditions
+                    .forCombat()
+                    .range(MAX_TARGET_DISTANCE)
+                    .selector(pTargetPredicate);
+        }
     }
 }
