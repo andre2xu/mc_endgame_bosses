841ce17225d551f228fad1f362ca65b0905681a4
andre2xu <>
2025-05-24 13:42:19 +0100

    Created a custom nearest attackable target goal for the Samurice and registered it. Registered Minecraft's hurt by target goal as well.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/SamuriceEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/SamuriceEntity.java
@@ -2,17 +2,26 @@ package com.github.andre2xu.endgamebosses.bosses.samurice;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.fluids.FluidType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
+
+import java.util.function.Predicate;
 
 public class SamuriceEntity extends PathfinderMob implements GeoEntity {
     /*
@@ -62,5 +71,38 @@ public class SamuriceEntity extends PathfinderMob implements GeoEntity {
     @Override
     public boolean canDrownInFluidType(FluidType type) {
         return false;
+    }
+
+    @Override
+    protected void registerGoals() {
+        // target the player that hurt the Samurice
+        this.targetSelector.addGoal(2, new HurtByTargetGoal(this, Player.class));
+
+        // find and select a target
+        this.targetSelector.addGoal(3, new SelectTargetGoal(this));
+    }
+
+
+
+    // CUSTOM GOALS
+    public static class SelectTargetGoal extends NearestAttackableTargetGoal<Player> {
+        public SelectTargetGoal(Mob pMob) {
+            // this is a custom constructor made to reduce the amount of parameters. It doesn't override any constructor from the parent
+
+            this(pMob, Player.class, 10, true, false, null);
+        }
+
+        public SelectTargetGoal(Mob pMob, Class<Player> pTargetType, int pRandomInterval, boolean pMustSee, boolean pMustReach, @Nullable Predicate<LivingEntity> pTargetPredicate) {
+            // this is the main constructor where the target conditions are set (see NearestAttackableTargetGoal). It was overridden to increase how far the Samurice can spot targets
+
+            super(pMob, pTargetType, pRandomInterval, pMustSee, pMustReach, pTargetPredicate);
+
+            final double MAX_TARGET_DISTANCE = 60d; // blocks
+            this.targetConditions = TargetingConditions
+                    .forCombat()
+                    .ignoreLineOfSight() // allow the Samurice to see a target even if there's obstructions in the way, e.g. trees, small hills, etc.
+                    .range(MAX_TARGET_DISTANCE)
+                    .selector(pTargetPredicate);
+        }
     }
 }
