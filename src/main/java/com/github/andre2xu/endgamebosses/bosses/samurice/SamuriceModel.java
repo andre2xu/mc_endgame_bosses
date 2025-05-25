5ba6e8f21bb1613db544c42d350fd9eca4684073
andre2xu <>
2025-05-25 12:21:37 +0100

    Overrode the 'setCustomAnimations' method of the Samurice's model class and implemented code to change the pitch of the head.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/SamuriceModel.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/SamuriceModel.java
@@ -2,6 +2,7 @@ package com.github.andre2xu.endgamebosses.bosses.samurice;
 
 import com.github.andre2xu.endgamebosses.EndgameBosses;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
 
 public class SamuriceModel extends GeoModel<SamuriceEntity> {
@@ -18,5 +19,14 @@ public class SamuriceModel extends GeoModel<SamuriceEntity> {
     @Override
     public ResourceLocation getAnimationResource(SamuriceEntity animatable) {
         return ResourceLocation.fromNamespaceAndPath(EndgameBosses.MODID, "animations/entity/samurice/samurice.animation.json");
+    }
+
+    @Override
+    public void setCustomAnimations(SamuriceEntity animatable, long instanceId, AnimationState<SamuriceEntity> animationState) {
+        super.setCustomAnimations(animatable, instanceId, animationState);
+
+        getBone("head").ifPresent(head -> {
+            head.setRotX(animatable.getHeadPitch());
+        });
     }
 }
