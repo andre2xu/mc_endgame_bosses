378f22dbbd37b0ba91caf879a6365cbf99583b0e
andre2xu <>
2025-05-26 11:39:13 +0100

    The Samurice can now jump, with guard up, when moving towards a target on the same y-level or when in a fluid.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/SamuriceEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/SamuriceEntity.java
@@ -243,10 +243,18 @@ public class SamuriceEntity extends PathfinderMob implements GeoEntity {
                         if (this.getY() == target.getY()) {
                             this.getNavigation().stop();
                             this.setDeltaMovement(vector_to_target.multiply(1, 0, 1).normalize().scale(0.2));
+
+                            if (this.horizontalCollision && this.onGround()) {
+                                this.jumpFromGround();
+                            }
                         }
                         else if (this.isInFluidType()) {
                             this.getNavigation().stop();
                             this.setDeltaMovement(vector_to_target.normalize().scale(0.1));
+
+                            if (this.horizontalCollision) {
+                                this.getJumpControl().jump();
+                            }
                         }
                         else {
                             this.getNavigation().moveTo(target, 0.4);
