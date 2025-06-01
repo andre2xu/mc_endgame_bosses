4a03c4ccba015fec63f0220e20e5f116aecfcf21
andre2xu <>
2025-06-01 12:38:27 +0100

    Created a Samurice clone entity and registered it as a misc entity.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/MiscEntityRegistry.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/MiscEntityRegistry.java
@@ -3,6 +3,7 @@ package com.github.andre2xu.endgamebosses.bosses;
 import com.github.andre2xu.endgamebosses.EndgameBosses;
 import com.github.andre2xu.endgamebosses.bosses.mama.egg_sac.MamaEggSacEntity;
 import com.github.andre2xu.endgamebosses.bosses.mama.spiderling.SpiderlingEntity;
+import com.github.andre2xu.endgamebosses.bosses.samurice.clone.SamuriceCloneEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,6 +20,8 @@ public class MiscEntityRegistry {
     public static RegistryObject<EntityType<MamaEggSacEntity>> MAMA_EGG_SAC = MISC_ENTITY_TYPES.register("mama_egg_sac", () -> EntityType.Builder.of(MamaEggSacEntity::new, MobCategory.MISC).sized(7f, 10f).build("mama_egg_sac"));
 
     public static RegistryObject<EntityType<SpiderlingEntity>> SPIDERLING = MISC_ENTITY_TYPES.register("spiderling", () -> EntityType.Builder.of(SpiderlingEntity::new, MobCategory.MISC).sized(3f, 1.3f).build("spiderling"));
+
+    public static RegistryObject<EntityType<SamuriceCloneEntity>> SAMURICE_CLONE = MISC_ENTITY_TYPES.register("samurice_clone", () -> EntityType.Builder.of(SamuriceCloneEntity::new, MobCategory.MISC).sized(0.5f, 2.5f).build("samurice_clone"));
     
     
     
---
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/clone/SamuriceCloneEntity.java
@@ -0,0 +1,40 @@
+package com.github.andre2xu.endgamebosses.bosses.samurice.clone;
+
+import com.github.andre2xu.endgamebosses.bosses.samurice.SamuriceEntity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+public class SamuriceCloneEntity extends SamuriceEntity {
+    public SamuriceCloneEntity(EntityType<? extends PathfinderMob> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    public static AttributeSupplier createAttributes() {
+        return PathfinderMob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 16)
+                .build();
+    }
+
+
+
+    // BOSS HEALTHBAR (disabled)
+    @Override
+    public void startSeenByPlayer(@NotNull ServerPlayer pServerPlayer) {}
+
+    @Override
+    public void stopSeenByPlayer(@NotNull ServerPlayer pServerPlayer) {}
+
+
+
+    // AI
+    @Override
+    protected void registerGoals() {}
+
+    @Override
+    public void aiStep() {}
+}
