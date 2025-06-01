bcff5b2d8c3e23603a63c8ab1effe51be55623ba
andre2xu <>
2025-06-01 13:16:53 +0100

    Replicated the Samurice's aiStep logic in the clone's aiStep method.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/clone/SamuriceCloneEntity.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/samurice/clone/SamuriceCloneEntity.java
@@ -3,13 +3,17 @@ package com.github.andre2xu.endgamebosses.bosses.samurice.clone;
 import com.github.andre2xu.endgamebosses.bosses.samurice.SamuriceEntity;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
 
 public class SamuriceCloneEntity extends SamuriceEntity {
     public SamuriceCloneEntity(EntityType<? extends PathfinderMob> pEntityType, Level pLevel) {
@@ -51,5 +55,99 @@ public class SamuriceCloneEntity extends SamuriceEntity {
     }
 
     @Override
-    public void aiStep() {}
+    public void aiStep() {
+        // NOTE: all the logic is the same as the parent's aiStep except there are no boss-related code
+
+        super.runBaseAiStep();
+
+        // heal in water
+        if (this.isInWater() && this.isAlive() && this.tickCount % 20 == 0) {
+            this.setHealth(this.getHealth() + 2); // 1 heart per second
+        }
+
+        // handle movement & attack decisions
+        LivingEntity target = this.getTarget();
+
+        if (target != null) {
+            boolean is_attacking = this.getAttackAction() != Action.Attack.NONE;
+
+            if (!is_attacking && !this.isBlockingAttacks()) {
+                this.getLookControl().setLookAt(target);
+
+                Vec3 target_pos = target.position();
+                Vec3 current_pos = this.position();
+
+                boolean same_xz_position_as_target = Math.abs(current_pos.x - target_pos.x) <= 0.5 && Math.abs(current_pos.z - target_pos.z) <= 0.5;
+
+                if (!this.isWithinGuardDistance(target)) {
+                    // put guard down before running
+                    if (this.isGuardUp()) {
+                        this.triggerAnim("movement_trigger_anim_controller", "guard_down");
+                        this.setIsGuardUp(false);
+                    }
+
+                    // run towards target
+                    if (this.chase_delay > 0) {
+                        this.chase_delay--;
+                    }
+                    else {
+                        this.followTarget(target, 0.6);
+
+                        if (!same_xz_position_as_target) {
+                            if (this.isInFluidType() && this.isUnderWater()) {
+                                this.triggerAnim("movement_trigger_anim_controller", "swim");
+                            }
+                            else {
+                                this.triggerAnim("movement_trigger_anim_controller", "run");
+                            }
+                        }
+
+                        // decide whether to dash towards target or not
+                        boolean should_dash_towards_target = new Random().nextInt(1, 11) == 1; // 1 in 10 chances
+
+                        if (should_dash_towards_target) {
+                            this.setAttackAction(Action.Attack.DASH);
+                        }
+                    }
+                }
+                else {
+                    // OBJECTIVE: When close to the target, put guard up and slowly move towards them
+
+                    if (!this.isGuardUp()) {
+                        this.triggerAnim("movement_trigger_anim_controller", "guard_up");
+                        this.setIsGuardUp(true);
+
+                        this.chase_delay = 20; // let target move far away for 1 second and then run towards them (this is for later)
+                    }
+                    else {
+                        if (this.distanceTo(target) > 3) {
+                            this.followTarget(target, 0.2);
+
+                            if (!same_xz_position_as_target) {
+                                this.triggerAnim("movement_trigger_anim_controller", "guard_up_move");
+                            }
+                        }
+                        else {
+                            // hold guard position when very close to the target
+                            this.triggerAnim("movement_trigger_anim_controller", "guard_up_stop_moving");
+
+                            // decide whether to attack or not
+                            boolean should_attack = new Random().nextInt(1, 11) == 1; // 1 in 10 chances
+
+                            if (should_attack) {
+                                this.setAttackAction(Action.Attack.CUTS);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            // put guard down since there's no target
+            if (this.isGuardUp()) {
+                this.triggerAnim("movement_trigger_anim_controller", "guard_down");
+                this.setIsGuardUp(false);
+            }
+        }
+    }
 }
