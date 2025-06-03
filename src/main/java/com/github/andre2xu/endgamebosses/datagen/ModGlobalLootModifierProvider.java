e545980c7c1849d840fe9229c30a397643658d93
andre2xu <>
2025-06-03 15:18:46 +0100

    Created a helper to make enchanted books in the global loot modifier provider. Added the mending book to the Mechalodon's drops.

--- src/main/java/com/github/andre2xu/endgamebosses/datagen/ModGlobalLootModifierProvider.java
+++ src/main/java/com/github/andre2xu/endgamebosses/datagen/ModGlobalLootModifierProvider.java
@@ -3,10 +3,14 @@ package com.github.andre2xu.endgamebosses.datagen;
 import com.github.andre2xu.endgamebosses.EndgameBosses;
 import com.github.andre2xu.endgamebosses.bosses.loot.BossLootModifier;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
@@ -20,6 +24,17 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
 
     public ModGlobalLootModifierProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, EndgameBosses.MODID, registries);
+    }
+
+    private ItemStack createEnchantedBook(HolderLookup.@NotNull Provider registries, CustomEnchantment[] enchantments) {
+        ItemStack enchanted_book = new ItemStack(Items.ENCHANTED_BOOK);
+
+        // add enchantments
+        for (CustomEnchantment enchantment : enchantments) {
+            registries.lookup(Registries.ENCHANTMENT).flatMap(action -> action.get(enchantment.name)).ifPresent(action2 -> enchanted_book.enchant(action2, enchantment.level));
+        }
+
+        return enchanted_book;
     }
 
     private void addBossDrops(String bossRegistryName, BossLoot[] drops) {
@@ -46,12 +61,16 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
                 new BossLoot("emerald", new ItemStack(Items.EMERALD), 0.7f, 12, 32),
                 new BossLoot("gold", new ItemStack(Items.GOLD_INGOT), 0.7f, 12, 32),
                 new BossLoot("diamond", new ItemStack(Items.DIAMOND), 0.5f, 6, 20),
-                new BossLoot("wither_skeleton_skull", new ItemStack(Items.WITHER_SKELETON_SKULL), 0.3f, 1, 2)
+                new BossLoot("wither_skeleton_skull", new ItemStack(Items.WITHER_SKELETON_SKULL), 0.3f, 1, 2),
+                new BossLoot("mending", this.createEnchantedBook(registries, new CustomEnchantment[] {
+                        new CustomEnchantment(Enchantments.MENDING, 1)
+                }), 0.1f, 1, 1)
         });
     }
 
 
 
-    private record BossLoot(String name, ItemStack item, float probability, int min, int max) {
-    }
+    private record BossLoot(String name, ItemStack item, float probability, int min, int max) {}
+
+    private record CustomEnchantment(ResourceKey<Enchantment> name, int level) {}
 }
