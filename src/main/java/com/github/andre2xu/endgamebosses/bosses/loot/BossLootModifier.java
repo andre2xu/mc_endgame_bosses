6cfa76276f218bf41c1a70a9849cf8bd502ebd13
andre2xu <>
2025-06-03 14:26:25 +0100

    The boss loot modifier now expects an ItemStack instead of an Item.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/loot/BossLootModifier.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/loot/BossLootModifier.java
@@ -4,8 +4,6 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
@@ -16,19 +14,19 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Random;
 
 public class BossLootModifier extends LootModifier {
-    private final Item custom_loot;
+    private final ItemStack custom_loot;
     private final int min_amount;
     private final int max_amount;
 
     public static final MapCodec<BossLootModifier> CODEC = RecordCodecBuilder.mapCodec(inst -> LootModifier.codecStart(inst)
-            .and(BuiltInRegistries.ITEM.byNameCodec().fieldOf("custom_loot").forGetter(modifier -> modifier.custom_loot))
+            .and(ItemStack.CODEC.fieldOf("custom_loot").forGetter(modifier -> modifier.custom_loot))
             .and(Codec.INT.fieldOf("min").forGetter(modifier -> modifier.min_amount))
             .and(Codec.INT.fieldOf("max").forGetter(modifier -> modifier.max_amount))
             .apply(inst, BossLootModifier::new)); // this codec specifies how to read the loot item's data from the loot table JSON file
 
 
 
-    public BossLootModifier(LootItemCondition[] conditionsIn, Item customLoot, int minAmount, int maxAmount) {
+    public BossLootModifier(LootItemCondition[] conditionsIn, ItemStack customLoot, int minAmount, int maxAmount) {
         super(conditionsIn);
 
         this.custom_loot = customLoot;
@@ -47,9 +45,10 @@ public class BossLootModifier extends LootModifier {
 
         // calculate the amount to drop
         int amount = new Random().nextInt(this.min_amount, this.max_amount + 1);
+        this.custom_loot.setCount(amount);
 
         // add custom loot to drops
-        generatedLoot.add(new ItemStack(this.custom_loot, amount));
+        generatedLoot.add(this.custom_loot);
 
         return generatedLoot;
     }
