101db2a795be87496a2b923d417a5811da74791e
andre2xu <>
2025-06-03 13:54:52 +0100

    Implemented drop amount in the boss loot modifier.

--- src/main/java/com/github/andre2xu/endgamebosses/bosses/loot/BossLootModifier.java
+++ src/main/java/com/github/andre2xu/endgamebosses/bosses/loot/BossLootModifier.java
@@ -1,5 +1,6 @@
 package com.github.andre2xu.endgamebosses.bosses.loot;
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
@@ -12,17 +13,27 @@ import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Random;
+
 public class BossLootModifier extends LootModifier {
     private final Item custom_loot;
+    private final int min_amount;
+    private final int max_amount;
 
-    public static final MapCodec<BossLootModifier> CODEC = RecordCodecBuilder.mapCodec(inst -> LootModifier.codecStart(inst).and(BuiltInRegistries.ITEM.byNameCodec().fieldOf("item").forGetter(modifier -> modifier.custom_loot)).apply(inst, BossLootModifier::new)); // this codec specifies how to read the loot item's data from the loot table JSON file
+    public static final MapCodec<BossLootModifier> CODEC = RecordCodecBuilder.mapCodec(inst -> LootModifier.codecStart(inst)
+            .and(BuiltInRegistries.ITEM.byNameCodec().fieldOf("custom_loot").forGetter(modifier -> modifier.custom_loot))
+            .and(Codec.INT.fieldOf("min").forGetter(modifier -> modifier.min_amount))
+            .and(Codec.INT.fieldOf("max").forGetter(modifier -> modifier.max_amount))
+            .apply(inst, BossLootModifier::new)); // this codec specifies how to read the loot item's data from the loot table JSON file
 
 
 
-    public BossLootModifier(LootItemCondition[] conditionsIn, Item customLoot) {
+    public BossLootModifier(LootItemCondition[] conditionsIn, Item customLoot, int minAmount, int maxAmount) {
         super(conditionsIn);
 
         this.custom_loot = customLoot;
+        this.min_amount = minAmount;
+        this.max_amount = maxAmount;
     }
 
     @Override
@@ -34,8 +45,11 @@ public class BossLootModifier extends LootModifier {
             }
         }
 
+        // calculate the amount to drop
+        int amount = new Random().nextInt(this.min_amount, this.max_amount + 1);
+
         // add custom loot to drops
-        generatedLoot.add(new ItemStack(this.custom_loot));
+        generatedLoot.add(new ItemStack(this.custom_loot, amount));
 
         return generatedLoot;
     }
